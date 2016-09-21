5b36a4d278d5870ddbd31e44705710a0b6f721d5
sirerr <sirerr@gmail.com>
2016-09-20 20:21:01 -0400

    added mouse look

--- unitycore/blackspot/Assets/scripts/player/CreateAndShootAction.cs
+++ unitycore/blackspot/Assets/scripts/player/CreateAndShootAction.cs
@@ -20,6 +20,14 @@ public class CreateAndShootAction : MonoBehaviour {
     private float holdballtime =0;
     public float holdballtimelimit = 10;
 
+	public bool usingMouse = false;
+
+	//using mouse
+	public bool mouseClickDown = false;
+	public bool mouseClickUp = false;
+	public bool mouseclickholding = false;
+	//using mouse
+
     void Awake()
     {
         trackedobj = GetComponent<SteamVR_TrackedObject>();
@@ -28,49 +36,105 @@ public class CreateAndShootAction : MonoBehaviour {
 
     void Update()
     {
-        var device = SteamVR_Controller.Input((int)trackedobj.index);
-        triggerpull = device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger);
-        triggerletgo = device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger);
-        triggerholding = device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+		if(usingMouse)
+		{
+			mouseClickDown = Input.GetMouseButtonDown(0);
+			mouseclickholding = Input.GetMouseButton(0);
+			mouseClickUp = Input.GetMouseButtonUp(0);
 
-        if (triggerpull && !ballmade)
-        {
-         //   print("trigger pulled");
-            GameObject newball = Instantiate(ballobj, spotlocation.position, Quaternion.identity) as GameObject;
-            newball.GetComponent<Rigidbody>().isKinematic = true;
-            newball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            newball.transform.parent = transform;
-            ballmade = true;
-            currentball = newball;
-        }
 
-        if (triggerholding && ballmade)
-        {
+			if(mouseClickDown && !ballmade)
+			{
+				//   print("trigger pulled");
+				GameObject newball = Instantiate(ballobj, spotlocation.position, Quaternion.identity) as GameObject;
+				newball.GetComponent<Rigidbody>().isKinematic = true;
+				newball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+				newball.transform.parent = transform;
+				ballmade = true;
+				currentball = newball;
+			}
 
-            holdballtime += Time.deltaTime;
+			if(mouseclickholding && ballmade)
+			{
+				holdballtime += Time.deltaTime;
 
-            if (holdballtime > holdballtimelimit)
-            {
-                Rigidbody rbody = currentball.GetComponent<Rigidbody>();
-                currentball.transform.parent = null;
-              rbody.isKinematic = false;
-               rbody.constraints = RigidbodyConstraints.None;
-                float speed = ballspeed * (10 * holdballtime);
-                rbody.AddForce(transform.forward *speed);
-                holdballtime = 0;
-            }
-        }
+				if (holdballtime > holdballtimelimit)
+				{
+					Rigidbody rbody = currentball.GetComponent<Rigidbody>();
+					currentball.transform.parent = null;
+					rbody.isKinematic = false;
+					rbody.constraints = RigidbodyConstraints.None;
+					float speed = ballspeed * (10 * holdballtime);
+					rbody.AddForce(transform.forward *speed);
+					holdballtime = 0;
+				}
 
-        if (triggerletgo && ballmade)
-        {
-            Rigidbody rbody = currentball.GetComponent<Rigidbody>();
-            currentball.transform.parent = null;
-            rbody.isKinematic = false;
-            rbody.constraints = RigidbodyConstraints.None;
-            float speed = ballspeed * (10 * holdballtime);
-            rbody.AddForce(transform.forward * speed);
-            holdballtime = 0;
-            ballmade = false;
-        }
-    }
+			}
+
+			if(mouseClickUp && ballmade)
+			{
+				
+					Rigidbody rbody = currentball.GetComponent<Rigidbody>();
+					currentball.transform.parent = null;
+					rbody.isKinematic = false;
+					rbody.constraints = RigidbodyConstraints.None;
+					float speed = ballspeed * (10 * holdballtime);
+					rbody.AddForce(transform.forward * speed);
+					holdballtime = 0;
+					ballmade = false;
+		
+
+			}
+		}
+		else
+		{
+			var device = SteamVR_Controller.Input((int)trackedobj.index);
+			triggerpull = device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger);
+			triggerletgo = device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger);
+			triggerholding = device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+
+			if (triggerpull && !ballmade)
+			{
+				//   print("trigger pulled");
+				GameObject newball = Instantiate(ballobj, spotlocation.position, Quaternion.identity) as GameObject;
+				newball.GetComponent<Rigidbody>().isKinematic = true;
+				newball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+				newball.transform.parent = transform;
+				ballmade = true;
+				currentball = newball;
+			}
+
+			if (triggerholding && ballmade)
+			{
+
+				holdballtime += Time.deltaTime;
+
+				if (holdballtime > holdballtimelimit)
+				{
+					Rigidbody rbody = currentball.GetComponent<Rigidbody>();
+					currentball.transform.parent = null;
+					rbody.isKinematic = false;
+					rbody.constraints = RigidbodyConstraints.None;
+					float speed = ballspeed * (10 * holdballtime);
+					rbody.AddForce(transform.forward *speed);
+					holdballtime = 0;
+				}
+			}
+
+			if (triggerletgo && ballmade)
+			{
+				Rigidbody rbody = currentball.GetComponent<Rigidbody>();
+				currentball.transform.parent = null;
+				rbody.isKinematic = false;
+				rbody.constraints = RigidbodyConstraints.None;
+				float speed = ballspeed * (10 * holdballtime);
+				rbody.AddForce(transform.forward * speed);
+				holdballtime = 0;
+				ballmade = false;
+			}
+		}
+
+	}
 }
+       
+
